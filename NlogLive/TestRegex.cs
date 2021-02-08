971bb0d4330c1b7e9db2cccdc897e55ad1d615ee
himanshunete <himanshuneteh@gmail.com>
2021-02-08 11:51:12 +0530

    [himanshunete]Add ValidateLastName

--- NlogLive/TestRegex.cs
+++ NlogLive/TestRegex.cs
@@ -9,7 +9,9 @@ namespace NlogLive
 
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         string firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+        string lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
         string[] patternfirstName = { "Himanshu", "Vineet" };
+        string[] patternlastName = { "Nete", "Kadwe" };
 
         public void firstName()
         {
@@ -29,12 +31,33 @@ namespace NlogLive
             Logger.Info(" Regex Expression is Successfully Executed ");
         }
 
+        public void lastName()
+        {
+            Regex regex = new Regex(lastNamePattern);
+            for (int i = 0; i < patternlastName.Length; i++)
+            {
+                var result = regex.Match(patternlastName[i]);
+                if (result.Success)
+                {
+                    Console.WriteLine(patternlastName[i] + " Its a Valid Pattern ");
+                }
+                else
+                {
+                    Console.WriteLine(patternlastName[i] + " Its not a Valid Pattern ");
+                }
+            }
+            Logger.Info(" Regex Expression is Successfully Executed ");
+        }
+
+
+
 
         static void Main(string[] args)
         {
             TestRegex regex = new TestRegex();
             regex.firstName();
             Console.WriteLine("**************************");
+            regex.lastName();
         }
     }
 }
